774d287d0947ea923fae9699a62efa9f2a1bc555
Andy Forceno <aforceno@protonmail.com>
2018-12-03 23:16:01 -0500

    2018-10-03 - Clickable word links

--- app/app.py
+++ app/app.py
@@ -8,13 +8,16 @@ import dictionary
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        word = request.form['word']
+
     wordlist = open('words.txt')
     line = next(wordlist)
     # Pick a random word from the word list file
     for num, line in enumerate(wordlist):
-      if random.randrange(num + 2): continue
+      if random.randrange(num +  2): continue
       word = line.rstrip("\n")
     wordlist.close()
 
@@ -22,6 +25,7 @@ def index():
     defin = dictionary.get_defin(word)
 
     return render_template('index.html', word=word, synonyms=words, definitions=defin)
+
 
 @app.route('/word/<word>', methods=['GET', 'POST'])
 def get_words(word):
@@ -38,4 +42,4 @@ def get_words(word):
 if __name__ == '__main__':
     app.config.update(TEMPLATES_AUTO_RELOAD=True)
     app.config['PREFERRED_URL_SCHEME'] = 'http'
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5555, threaded=True) #, debug=True
